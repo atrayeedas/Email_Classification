63739c6c4f731286f36bb4e7737cae1b4bb6566b
atrayeedas <atrayee290@gmail.com>
2023-06-28 23:34:30 +0530

    Flask

--- src/FilterModel/maintask.py
+++ src/FilterModel/maintask.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/")
+"""@app.route("/")
 def start():
-    return render_template("index.html")
+    return render_template("index.html")"""
 
 
 @app.route("/spamcheck", methods=["POST", "GET"])
